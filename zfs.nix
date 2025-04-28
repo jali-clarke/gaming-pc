405412fe11aeef6d1ee140637daca914ec899302
jali-clarke <GRX01@hotmail.com>
2025-04-27 20:15:31 -0400

    mount persistent zfs dataset to home

--- zfs.nix
+++ zfs.nix
@@ -5,4 +5,9 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = false;
   networking.hostId = "b09975b1"; # head -c4 /dev/urandom | od -A none -t x4
+
+  fileSystems."/home/pi" = {
+    device = "zfs-fast/home-persistent";
+    fsType = "zfs";
+  };
 }
