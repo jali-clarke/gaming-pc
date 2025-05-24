12f55c9fbac85e9c0f16c9de03d43d79ec506b01
jali-clarke <GRX01@hotmail.com>
2025-05-23 22:59:23 -0400

    disable packages we are not using

--- gnome.nix
+++ gnome.nix
@@ -1,6 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  # `gnome` comes with a bunch of packages installed by default.
+  # we don't need em all.  see a list here (change to whatever
+  # release branch you're using):
+  # https://github.com/NixOS/nixpkgs/blob/2baa12ff69913392faf0ace833bc54bba297ea95/nixos/modules/services/x11/desktop-managers/gnome.nix#L422
+  environment.gnome.excludePackages = [
+    pkgs.epiphany
+    pkgs.gnome-contacts
+    pkgs.gnome-text-editor
+    pkgs.gnome-maps
+    pkgs.gnome-music
+  ];
 }
