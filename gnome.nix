45f2b2daaf444504a3e71338c84c65488dfc8d74
jali-clarke <GRX01@hotmail.com>
2025-12-14 13:21:10 -0500

    update to nixpkgs 25.11

--- gnome.nix
+++ gnome.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # `gnome` comes with a bunch of packages installed by default.
   # we don't need em all.  see a list here (change to whatever
